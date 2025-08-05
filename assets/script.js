21c47673ff7f9eb9a4d733ea2e4a31daecff4f22
William Hare <billman311@aol.com>
2025-08-05 12:22:38 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,24 @@
 jQuery(document).ready(function($) {
-    $('#wc-sales-tax-cal-notice .notice-dismiss').on('click', function() {
+    console.log('WC Sales Tax Cal script loaded.');
+    $(document).on('click', '#wc-sales-tax-cal-notice .notice-dismiss', function(e) {
+        e.preventDefault();
+        console.log('Dismiss button clicked.');
+        var notice = $(this).closest('#wc-sales-tax-cal-notice');
         $.post(wcSalesTaxCal.ajax_url, {
             action: 'wc_sales_tax_cal_dismiss',
             nonce: wcSalesTaxCal.nonce
         }, function(response) {
+            console.log('AJAX response:', response);
             if (response.success) {
-                $('#wc-sales-tax-cal-notice').fadeOut();
+                console.log('Dismiss successful.');
+                notice.fadeOut(400, function() {
+                    notice.remove();
+                });
+            } else {
+                console.error('Dismiss failed:', response);
             }
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.error('AJAX error:', textStatus, errorThrown);
         });
     });
 });
